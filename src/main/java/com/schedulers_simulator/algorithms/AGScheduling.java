f7ccd0010012d80495d5c07683e739eb97a18fed
Youssef Moataz <yousefcrunshy03@gmail.com>
2023-12-12 14:55:12 +0200

    Adding AG scheduling algorithm.

---
+++ src/main/java/com/schedulers_simulator/algorithms/AGScheduling.java
@@ -0,0 +1,14 @@
+package com.schedulers_simulator.algorithms;
+
+public class AGScheduling implements Algorithm{
+    @Override
+    public void run() {
+
+    }
+
+    public static void main(String[] args) {
+        Algorithm algorithm = new AGScheduling();
+        algorithm.run();
+    }
+    
+}
